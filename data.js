eba69b842bd8f6cb567190e45ea4d1a558c8ece6
Fernando Gonzalez <84160418+FernandoNGonzalez@users.noreply.github.com>
2024-01-15 00:14:58 -0300

    Primer commit

---
+++ data.js
@@ -0,0 +1,11 @@
+const libros = [
+  { id: 1, titulo: 'The little Prince', autor: 'Antoine de Saint-Exupéry' },
+  {
+    id: 2,
+    titulo: 'Harry Potter and the Philosophers Stone',
+    autor: 'J. K. Rowling.',
+  },
+  { id: 3, titulo: 'The Chronicles of Narnia', autor: 'C. S. Lewis' },
+];
+
+module.exports = libros;
